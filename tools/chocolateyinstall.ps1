55ac5a0f7ceee99800e46c14b440e548fe0626f4
Kim Walisch <kim.walisch@gmail.com>
2018-01-13 12:50:24 +0100

    Support 32-bit

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64      = 'https://github.com/kimwalisch/primesieve/releases/download/v6.2/primesieve-6.2-win64-console.zip'
 $url        = 'https://github.com/kimwalisch/primesieve/releases/download/v3.6/primesieve-3.6-win32-console.zip'
+$osBitness  = Get-ProcessorBits
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -21,4 +22,11 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyZipPackage $packageName $url64 -checksum64 $checksum64 -checksumType64 $checksumType64 $toolsDir
+if ($osBitness -eq 64)
+{
+  Install-ChocolateyZipPackage $packageName $url64 -checksum64 $checksum64 -checksumType64 $checksumType64 $toolsDir
+}
+else
+{
+  Install-ChocolateyZipPackage $packageName $url -checksum $checksum -checksumType $checksumType $toolsDir
+}
