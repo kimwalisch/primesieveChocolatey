6edc14def2ce1b41c77de10d91333b6bcb5b9929
Kim Walisch <kim.walisch@gmail.com>
2018-01-14 11:15:49 +0100

    Include binaries into package

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,14 @@
-$packageName = $env:ChocolateyPackageName
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/kimwalisch/primesieve/releases/download/v6.2/primesieve-6.2-win64-console.zip'
-$url         = 'https://github.com/kimwalisch/primesieve/releases/download/v3.6/primesieve-3.6-win32-console.zip'
-$checksum    = '0e08cad6beecf21629b81074307c88ae0794252cd86bc5c2b14029dbc5128574'
-$checksum64  = '366cb96c596073a3211de6eb2292da2be806c4521bd35a652a5bf3430c82641c'
+$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName `
-                             -FileType 'exe' `
-                             -Url $url `
-                             -Url64bit $url64 `
-                             -Checksum $checksum `
-                             -Checksum64 $checksum64 `
-                             -ChecksumType 'sha256' `
-                             -SilentArgs '/VERYSILENT' `
-                             -ValidExitCodes @(0) `
-                             -UnzipLocation $toolsDir
+$packageArgs = @{
+    PackageName    = $env:ChocolateyPackageName
+    FileType       = 'exe'
+    SoftwareName   = 'Primesieve*'
+    File           = gi $toolsDir\primesieve-3.6-win32-console\primesieve.exe
+    File64         = gi $toolsDir\primesieve-6.2-win64-console\primesieve.exe
+    SilentArgs     = "1000000 -q"
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
