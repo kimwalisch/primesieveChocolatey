dcb06162e067726587a95df1c7036d53196e36a6
Kim Walisch <kim.walisch@gmail.com>
2018-01-14 16:30:17 +0100

    Improved installation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,12 @@
 $ErrorActionPreference = 'Stop'
-$toolsDir  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$osBitness = Get-OSArchitectureWidth
-$zipFile   = gi $toolsDir\primesieve-*-win$osBitness-console.zip
-
-# Unzip either 32-bit or 64-bit primesieve-*.zip
-Get-ChocolateyUnzip -FileFullPath $zipFile -Destination $toolsDir
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits     = Get-OSArchitectureWidth
+$zipFile  = gi $toolsDir\primesieve-*-win$bits-console.zip
 
 $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
-    FileType       = 'exe'
-    SoftwareName   = 'Primesieve*'
-    File           = gi $toolsDir\primesieve.exe
-    File64         = gi $toolsDir\primesieve.exe
-    SilentArgs     = "1000000 -q"
-    validExitCodes = @(0)
+    Destination    = $toolsDir
+    File           = $zipFile
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
