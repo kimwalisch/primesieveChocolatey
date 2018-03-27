200fd51f904a86b229d18c6205b9261651a8c6ee
Kim Walisch <kim.walisch@gmail.com>
2018-03-27 19:50:16 +0200

    Remove old 32-bit binary

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     PackageName = $env:ChocolateyPackageName
     Destination = $toolsDir
-    File        = gi $toolsDir\primesieve-*-win32-console.zip
     File64      = gi $toolsDir\primesieve-*-win64-console.zip
 }
 
